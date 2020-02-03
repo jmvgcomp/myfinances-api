302a8208d61704d8e2299842495c1468efd237e2
Lenk <jmvg.comp@gmail.com>
2020-02-03 01:07:56 -0300

    First commit

---
+++ src/main/java/dev/jmvg/myfinances/MyfinanceApiApplication.java
@@ -0,0 +1,13 @@
+package dev.jmvg.myfinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfinanceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/jmvg/myfinances/myfinanceapi/MyfinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jmvg.myfinances.myfinanceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
